386a316d980d8cbdc26d1a7c7da31b1d9dbe5d9b
Luthien-in-edhil <luthien@parendili.org>
2019-12-19 13:12:33 +0100

    preliminary workaround

--- src/main/java/eu/europeana/keycloak/apikey/ApikeySynchronizer.java
+++ src/main/java/eu/europeana/keycloak/apikey/ApikeySynchronizer.java
@@ -7,6 +7,8 @@ import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.impl.client.CloseableHttpClient;
+//import org.keycloak.adapters.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicHeader;
 import org.slf4j.Logger;
